9246f2da22efdf9ea8e685c3c389e8f7f5b8ae11
shu-tom <iamabigman189@gmail.com>
2018-01-17 13:24:34 +0900

    Updated redleavesscan.py. Supported the new version RedLeaves. (Himawari, Armadill and Lavender)

--- redleavesscan.py
+++ redleavesscan.py
@@ -28,10 +28,29 @@ redleaves_sig = {
                     strings: \
                        $v1 = "red_autumnal_leaves_dllmain.dll" \
                        $b1 = { FF FF 90 00 } \
-                    condition: $v1 and $b1 at 0}'
+                    condition: $v1 and $b1 at 0}',
+    'namespace2' : 'rule Himawari { \
+                    strings: \
+                       $h1 = "himawariA" \
+                       $h2 = "himawariB" \
+                       $h3 = "HimawariDemo" \
+                    condition: $h1 and $h2 and $h3}',
+    'namespace3' : 'rule Lavender { \
+                    strings: \
+                       $l1 = {C7 ?? ?? 4C 41 56 45} \
+                       $l2 = {C7 ?? ?? 4E 44 45 52} \
+                    condition: $l1 and $l2}',
+    'namespace4' : 'rule Armadill { \
+                    strings: \
+                       $a1 = {C7 ?? ?? 41 72 6D 61 } \
+                       $a2 = {C7 ?? ?? 64 69 6C 6C } \
+                    condition: $a1 and $a2}'
 }
 
-CONF_PATTERNS = [["RedLeaves", re.compile("\x68\x88\x13\x00\x00\xFF", re.DOTALL)]
+CONF_PATTERNS = [["RedLeaves", re.compile("\x68\x88\x13\x00\x00\xFF", re.DOTALL)],
+                 ["Himawari", re.compile("\x68\x70\x03\x00\x00\xBF", re.DOTALL)],
+                 ["Lavender", re.compile("\x68\x70\x03\x00\x00\xBF", re.DOTALL)],
+                 ["Armadill", re.compile("\x68\x70\x03\x00\x00\xBF", re.DOTALL)],
                  ]
 
 CONNECT_MODE = {1 : 'TCP' , 2 : 'HTTP', 3 : 'HTTPS', 4 : 'TCP and HTTP'}
@@ -117,17 +136,8 @@ class redleavesConfig(redleavesScan):
         injection = unpack_from('<104s', cfg_blob, 0x726)[0]
         rc4key = unpack_from('<10s', cfg_blob, 0x82A)[0]
 
-        if "exe" not in injection.replace('\0',''):
-            injection = list(injection)
-            i = 0
-            for s in injection:
-                xor = ord(s) ^ ord(injection[-1])
-                injection[i] = chr(xor)
-                i += 1
-            injection = "".join(injection)
-
         ## config write
-        outfd.write("[RedLeaves Config Info]\n")
+        outfd.write("[Config Info]\n")
         outfd.write("Server1\t\t\t: %s\n" % server1.split('\0')[0])
         outfd.write("Server2\t\t\t: %s\n" % server2.split('\0')[0])
         outfd.write("Server3\t\t\t: %s\n" % server3.split('\0')[0])
@@ -138,30 +148,63 @@ class redleavesConfig(redleavesScan):
         outfd.write("Injection Process\t: %s\n" % injection.replace('\0',''))
         outfd.write("RC4 Key\t\t\t: %s\n" % rc4key.split('\0')[0])
 
+    def parse_config_himawari(self, cfg_blob, cfg_sz, cfg_addr, outfd):
+        server1 = unpack_from('<64s', cfg_blob, 0x4)[0]
+        server2 = unpack_from('<64s', cfg_blob, 0x44)[0]
+        server3 = unpack_from('<64s', cfg_blob, 0x84)[0]
+        server4 = unpack_from('<64s', cfg_blob, 0xC4)[0]
+        port = unpack_from('<I', cfg_blob, 0x104)[0]
+        unknown1 = unpack_from('<64s', cfg_blob, 0x10C)[0]
+        unknown2 = unpack_from('<64s', cfg_blob, 0x14C)[0]
+        unknown3 = unpack_from('<64s', cfg_blob, 0x18C)[0]
+        sleep1 = unpack_from('<I', cfg_blob, 0x1D0)[0]
+        sleep2 = unpack_from('<I', cfg_blob, 0x1D4)[0]
+        mode = unpack_from('<I', cfg_blob, 0x1D8)[0]
+        id = unpack_from('<64s', cfg_blob, 0x1E0)[0]
+        mutex = unpack_from('<62s', cfg_blob, 0x224)[0]
+        sleep3 = unpack_from('<I', cfg_blob, 0x220)[0]
+        ua = unpack_from('<260s', cfg_blob, 0x262)[0]
+        key = unpack_from('<10s', cfg_blob, 0x366)[0]
+
+        ## config write
+        outfd.write("[Config Info]\n")
+        outfd.write("Server1\t\t\t: %s\n" % server1.split('\0')[0])
+        outfd.write("Server2\t\t\t: %s\n" % server2.split('\0')[0])
+        outfd.write("Server3\t\t\t: %s\n" % server3.split('\0')[0])
+        outfd.write("Server4\t\t\t: %s\n" % server4.split('\0')[0])
+        outfd.write("Port\t\t\t: %i\n" % port)
+        outfd.write("Mode\t\t\t: %i \n" % (mode))
+        outfd.write("ID\t\t\t: %s\n" % id.split('\0')[0])
+        outfd.write("Mutex\t\t\t: %s\n" % mutex.replace('\0',''))
+        outfd.write("Key\t\t\t: %s\n" % key.split('\0')[0])
+        outfd.write("UserAgent\t\t\t: %s\n" % ua.split('\0')[0])
+        outfd.write("unknown1\t\t\t: %s\n" % repr(unknown1))
+        outfd.write("unknown2\t\t\t: %s\n" % repr(unknown2))
+        outfd.write("unknown3\t\t\t: %s\n" % repr(unknown3))
+
     def render_text(self, outfd, data):
 
         delim = '-' * 70
 
         for task, start, malname, memory_model in data:
-            if "RedLeaves" in str(malname):
-                proc_addr_space = task.get_process_address_space()
+            proc_addr_space = task.get_process_address_space()
 
-                data = proc_addr_space.zread(start, vad_ck().get_vad_end(task, start)-start)
+            data = proc_addr_space.zread(start, vad_ck().get_vad_end(task, start)-start)
 
-                loadp = patternCheck(malname ,data)
-                if loadp.m_conf is None:
-                    continue
+            loadp = patternCheck(malname ,data)
+            if loadp.m_conf is None:
+                continue
 
-                offset_conf = loadp.m_conf.start()
+            offset_conf = loadp.m_conf.start()
+
+            if str(malname) in "RedLeaves":
                 config_size = 2100
 
                 offset_conf -= 1
                 while data[offset_conf]!="\xC7" and data[offset_conf]!="\xBE" and data[offset_conf]!="\xBF":
                     offset_conf -= 1
-
                 if data[offset_conf]!="\xC7" and data[offset_conf]!="\xBE" and data[offset_conf]!="\xBF":
                     continue
-
                 if data[offset_conf]=="\xC7" and data[offset_conf + 1]!="\x85" and data[offset_conf + 1]!="\x45":
                     offset_conf -= 6
 
@@ -172,16 +215,33 @@ class redleavesConfig(redleavesScan):
                     (config_addr, ) = unpack("=I", data[offset_conf+6:offset_conf+10])
                 else:
                     (config_addr, ) = unpack("=I", data[offset_conf+1:offset_conf+5])
+                outfd.write("Process: %8X\n\n" % offset_conf)
+
+                if config_addr < start:
+                    continue
+                outfd.write("{0}\n".format(delim))
+                outfd.write("RedLeaves Settings:\n\n")
+                config_addr -= start
+                config_data = data[config_addr:config_addr+config_size]
+                outfd.write("Process: %s (%d)\n\n" % (task.ImageFileName, task.UniqueProcessId))
+                if len(config_data) > 0:
+                    self.parse_config(config_data, config_size, config_addr, outfd)
+
+            if str(malname) in ["Himawari", "Lavender", "Armadill"]:
+                offset_conf += 6
+                config_size = 880
+
+                # get address
+                (config_addr, ) = unpack("=I", data[offset_conf:offset_conf+4])
+                outfd.write("config addr: %08X\n\n" % config_addr)
 
                 if config_addr < start:
                     continue
 
                 outfd.write("{0}\n".format(delim))
-                outfd.write("RedLeaves Settings:\n\n")
-
+                outfd.write("%s Settings:\n\n" % str(malname))
                 config_addr -= start
                 config_data = data[config_addr:config_addr+config_size]
                 outfd.write("Process: %s (%d)\n\n" % (task.ImageFileName, task.UniqueProcessId))
-
                 if len(config_data) > 0:
-                    self.parse_config(config_data, config_size, config_addr, outfd)
+                    self.parse_config_himawari(config_data, config_size, config_addr, outfd)
